005eea59035f5454576578928f9eba6934e3c6e4
Arghya <arghyahari2001@gmail.com>
2024-05-06 15:30:04 +0530

    customhook useCounter added

---
+++ src/reacthooks/useCounter.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const useCounter = () => {
+    const initialState=0;
+    const[count,setCount]=useState(initialState);
+
+    function Increment(){
+        setCount(count+1);
+    }
+
+    function Decrement(){
+        setCount(count-1);
+    }
+
+
+
+  return [count,Increment,Decrement];
+    
+  
+}
+
+export default useCounter
